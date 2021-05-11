0c8b2cef4dc767112310b55b4b4700a3889d1a12
ksemdm-art <81703860+ksemdm-art@users.noreply.github.com>
2021-05-11 05:59:35 +0300

    Add files via upload

--- Frontend/js/personal.js
+++ Frontend/js/personal.js
@@ -1,17 +1,42 @@
-function dropDown() {
-    document.getElementById("myDropdown").classList.toggle("show");
-}
+window.onload = () =>{
+    let input = document.querySelector('#input');
+    input.oninput = function(){
+        let value = this.value.trim();
+        let list = document.querySelectorAll('.for-ul li')
+        
+        if (value != ''){
+            list.forEach(elem => {
+                if(elem.innerText.search(value) == -1){
+                    elem.classList.add('hide');
+                    elem.innerHTML = elem.innerText;
+                }
+                else{
+                    elem.classList.remove('hide');
+                    let str = elem.innerText;
+                    elem.innerHTML = insertMark(str, elem.innerText.search(value), value.length);
+                }
+            });
+        }
+        else{
+            list.forEach(elem => {
+                elem.classList.remove('hide');
+                elem.innerHTML = elem.innerText;
 
-window.onclick = function(event) {
-if (!event.target.matches('.dropbtn')) {
+            });
+        }
+    };
 
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-    var openDropdown = dropdowns[i];
-    if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+    function insertMark(string, pos, len){
+        return string.slice(0, pos)+'<mark>'+string.slice(pos, pos+len)+'</mark>'+string.slice(pos+len);
+
     }
-    }
-}
-}
+};
+
+var height = 0;
+$('.for-ul li').each(function() {
+  if ($(this).index() < 4) {
+    height = height + $(this).outerHeight();
+    console.log(height);
+  }
+});
+$('.for-ul').css('max-height', height + 'px');
